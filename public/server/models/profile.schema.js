1a09ff5513ad1de554deb41115a54e891211b12d
Anantha Shankar Arun Kumar <anantha.shankar1990@gmail.com>
2016-04-14 07:32:22 -0400

    Session Management
    Implemented features to handle sessions using ngIdle. Any sessions idle
    more than 10 mins will be automatically logged of. Implemented cookie
    feature so that Page refresh will not tend to log of and will retain
    user details using ngStorage - sessionStorage. Modified image upload
    feature to directly store the image in MongoDB in base64 format.
    Changed UI parameters to display default images and usernames. Changed
    the schema of profile to add ‘Privacy’  and ‘Role’ attributes. Changed
    the date fields default value. Implemented feature to devaluate session
    on logout and restrict user from access account after logout.
    Implemented sha1 protocol to encrypt password to make application and
    its data more secure.

--- public/server/models/profile.schema.js
+++ public/server/models/profile.schema.js
@@ -5,7 +5,8 @@ module.exports = function (mongoose,db) {
 
     var ProfileSchema = new mongoose.Schema({
         userid:String,
-        profilePic:{ data: Buffer, contentType: String },
+        //profilePic:{ name: String, img: Buffer, contentType: String },
+        profilePic:String,
         friends:[{userid:String}],
         posts:[{postid:String}],
         messages:[{messageid:String}],
@@ -14,6 +15,8 @@ module.exports = function (mongoose,db) {
         country:String,
         occupation:String,
         friendRequests:[{userid:String}],
+        role: { type: String, default: 'user' },
+        privacy: {type: String, default: 'public'},
         dateOfBirth:{ type : Date, default: Date.now }
 
     },{collection:"profiles"});
